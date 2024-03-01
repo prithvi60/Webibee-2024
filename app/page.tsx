f2d52dc01640dce697263bed309d4783d61ff4c8
Prithvi <prithvin19@gmail.com>
2024-03-01 16:22:39 +0530

    remove parallaz

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,15 @@ import Benefits from "@/components/Benefits";
 import Contact from "@/components/Contact";
 import FAQ from "@/components/FAQ";
 import Hero from "@/components/Hero";
-const SuspenseComponent = dynamic(() => import("@/components/ParallaxHero"), {
-  suspense: true,
-  ssr: true,
-});
+// const SuspenseComponent = dynamic(() => import("@/components/ParallaxHero"), {
+//   suspense: true,
+//   ssr: true,
+// });
 // import ParallaxHero from '@/components/ParallaxHero';
 import Portfolio from "@/components/Portfolio";
 import Testimonials from "@/components/Testimonials";
 import Workflow from "@/components/Workflow";
-import dynamic from "next/dynamic";
+// import dynamic from "next/dynamic";
 
 export default function Home() {
   return (

e252082127ceb0b5ce88c5589a6108ca2deb94ac
Prithvi <prithvin19@gmail.com>
2024-06-02 14:16:18 +0530

    terms

---
+++ components/Terms.tsx
@@ -0,0 +1,257 @@
+import React from "react";
+
+export default function Terms() {
+  return (
+    <section className="bg-primary min-h-screen w-full flex gap-8 flex-col justify-center items-center text-2xl text-default p-8">
+      <h1 className="text-4xl font-bold mt-16">
+        INDEPENDENT CONTRACTOR MASTER SERVICES AGREEMENT
+      </h1>
+
+      <section className="flex flex-col gap-4">
+        <h2 className="text-3xl font-bold mb-6">1. Services</h2>
+        <p>
+          For all purposes of this MSA hereon, the "Independent Contractor"
+          shall be Revenueholic or any of its affiliates, and the counterparty
+          shall be the "Company". Company retains Independent Contractor, and
+          Independent Contractor agrees to perform for Company, certain services
+          set forth, notwithstanding any other document, in the signed order
+          form.
+        </p>
+      </section>
+
+      <section className="flex flex-col gap-4">
+        <h2 className="text-3xl font-bold mb-6">
+          2. Consideration / Compensation
+        </h2>
+        <p>
+          (a) Company shall provide Independent Contractor, as full and complete
+          and non-refundable compensation to be paid as agreed on the order
+          form. Payment will be due on receipt of the invoice. If an invoice is
+          not disputed within 3 business days from receipt, it shall be
+          considered final and binding.
+        </p>
+        <p>
+          (b) Independent Contractor is not entitled to receive any other
+          compensation or any benefits from the Company. Except as otherwise
+          required by law, Company shall not withhold any sums or payments made
+          to Independent Contractor for social security or other national,
+          state, or local tax liabilities or contributions, and all
+          withholdings, liabilities, and contributions shall be solely
+          Independent Contractor’s responsibility. Independent Contractor
+          further understands and agrees that the Services are not covered under
+          the unemployment compensation laws and are not intended to be covered
+          by workers’ compensation laws.
+        </p>
+      </section>
+
+      <section className="flex flex-col gap-4">
+        <h2 className="text-3xl font-bold mb-6">3. Nondisclosure</h2>
+        <p>
+          (a) Independent Contractor understands that, in connection with its
+          engagement with Company, it may receive, produce, or otherwise be
+          exposed to Company’s trade secrets, business, proprietary and/or
+          technical information, including, without limitation, information
+          concerning customer lists, customer support strategies, employees,
+          research and development, financial information (including sales,
+          costs, profits, and pricing methods), manufacturing, marketing,
+          proprietary software, hardware, firmware, and related documentation,
+          inventions (whether patentable or not), know-how, show-how, and other
+          information considered to be confidential by Company, and all
+          derivatives, improvements and enhancements to any of the above
+          (including those derivatives, improvements and enhancements that were
+          created or developed by Independent Contractor under this Agreement),
+          in addition to all information Company receives from others under an
+          obligation of confidentiality (individually and collectively
+          “Confidential Information”).
+        </p>
+        <p>
+          (b) Independent Contractor acknowledges that the Confidential
+          Information is the Company’s sole, exclusive and extremely valuable
+          property. Accordingly, Independent Contractor agrees to segregate all
+          Confidential Information from information of other companies and
+          agrees not to reproduce any Confidential Information without Company’s
+          prior written consent, not to use the Confidential Information except
+          in the performance of this Agreement, and not to divulge all or any
+          part of the Confidential Information in any form to any third party,
+          either during or after the term of this Agreement, except to Company
+          employees and Independent Contractor Employees who need to know such
+          Confidential Information in order to perform the Services. Independent
+          Contractor shall require such Independent Contractor Employees to
+          execute a non-disclosure agreement satisfactory to the Company before
+          such Independent Contractor Employee is exposed to any Confidential
+          Information. Upon termination or expiration of this Agreement for any
+          reason, Independent Contractor agrees to cease using and to return to
+          Company all whole and partial copies and derivatives of the
+          Confidential Information, whether in Independent Contractor’s
+          possession or under Independent Contractor’s direct or indirect
+          control, including any computer access nodes and/or codes, and to
+          arrange for the return of such materials by all Independent Contractor
+          Employees.
+        </p>
+        <p>
+          (c) Independent Contractor shall not disclose or otherwise make
+          available to Company in any manner any confidential and proprietary
+          information received by Independent Contractor from third parties.
+          Independent Contractor warrants that its performance of all the terms
+          of this Agreement does not and will not breach any agreement entered
+          into by Independent Contractor with any other party.
+        </p>
+      </section>
+
+      <section className="flex flex-col gap-4">
+        <h2 className="text-3xl font-bold mb-6">
+          4. Indemnification / Release
+        </h2>
+        <p>
+          (a) Independent Contractor agrees to take all necessary precautions to
+          prevent injury to any persons or damage to property during the term of
+          this Agreement, and shall indemnify, defend and hold harmless Company,
+          its officers, directors, shareholders, employees, representatives
+          and/or agents from any claim, liability, loss, cost, damage, judgment,
+          settlement or expense (including attorney’s fees) resulting from or
+          arising in any way out of injury (including death) to any person or
+          damage to property arising in any way out of any act, error, omission
+          or negligence on the part of Independent Contractor or any Independent
+          Contractor Employee in the performance or failure to fulfill any
+          Services or obligations under this Agreement.
+        </p>
+      </section>
+
+      <section className="flex flex-col gap-4">
+        <h2 className="text-3xl font-bold mb-6">
+          5. Insurance and Other Requirements
+        </h2>
+        <p>
+          (a) Independent Contractor warrants that it will obtain and keep in
+          full force and effect at all times hereunder workers’ compensation,
+          general liability and errors and omissions or professional liability
+          insurance covering all of its Services. Company shall be listed as an
+          additional named insured and/or as an additional loss-payee under such
+          policies.
+        </p>
+        <p>
+          (b) Independent Contractor shall provide to the Company copies of all
+          policies required to be maintained, and a Certificate of Insurance
+          indicating said coverage shall be provided to Company upon request.
+        </p>
+        <p>
+          (c) Independent Contractor also warrants and represents that it has
+          properly classified all of its workers, has and will maintain all
+          required licenses and certifications.
+        </p>
+      </section>
+
+      <section className="flex flex-col gap-4">
+        <h2 className="text-3xl font-bold mb-6">6. Renewal & Termination</h2>
+        <p>
+          This Agreement shall be effective on the date hereof and shall
+          continue until terminated by either party upon 30 business day written
+          notice. Unless mentioned otherwise, this agreement will be subject to
+          auto-renewal if no notice is provided in writing. Charges are subject
+          to increase depending upon usage.
+        </p>
+      </section>
+
+      <section className="flex flex-col gap-4">
+        <h2 className="text-3xl font-bold mb-6">7. Independent Contractor</h2>
+        <p>
+          (a) Company and Independent Contractor expressly agree and understand
+          that Independent Contractor is an independent contractor and nothing
+          in this Agreement shall be construed in any way or manner, to create
+          between them a relationship of employer and employee, principal and
+          agent, partners or any other relationship other than that of
+          independent parties contracting with each other solely for the purpose
+          of carrying out the provisions of the Agreement. Accordingly,
+          Independent Contractor acknowledges that Independent Contractor and
+          Independent Contractor’s Employees are not eligible for any Company
+          benefits, including, but not limited to, health insurance, retirement
+          plans or stock option plans. Independent Contractor is not the agent
+          of Company and is not authorized and shall not have the power or
+          authority to bind Company or incur any liability or obligation, or act
+          on behalf of Company. At no time shall Independent Contractor
+          represent that it is an agent of the Company, or that any of the
+          views, advice, statements and/or information that may be provided
+          while performing the Services are those of the Company.
+        </p>
+        <p>
+          (b) Independent Contractor is solely responsible for directing and
+          controlling the performance of the Services, including the time, place
+          and manner in which the Services are performed. Independent Contractor
+          shall use its best efforts, energy and skill in its own name and in
+          such manner as it sees fit.
+        </p>
+      </section>
+
+      <section className="flex flex-col gap-4">
+        <h2 className="text-3xl font-bold mb-6">8. General</h2>
+        <p>
+          (a) This Agreement does not create an obligation on Company to
+          continue to retain Independent Contractor beyond this Agreement’s
+          termination. This Agreement may not be changed unless mutually agreed
+          upon in writing by both parties.
+        </p>
+        <p>
+          (b) Independent Contractor hereby agrees that any breach of Section 3
+          by Independent Contractor will cause irreparable harm to Company and
+          that in the event of such breach or threatened breach, Company shall
+          have, in addition to any and all remedies of law and those remedies
+          stated in this Agreement, the right to an injunction, specific
+          performance or other equitable relief to prevent the violation of
+          Independent Contractor’s obligations hereunder.
+        </p>
+        <p>
+          (c) Independent Contractor hereby agrees that each provision herein
+          shall be treated as a separate and independent clause, and the
+          unenforceability of any one clause shall in no way impair the
+          enforceability of any of the other clauses herein.
+        </p>
+        <p>
+          (d) This Agreement contains the entire agreement between the parties
+          hereto with respect to the transactions contemplated herein. The
+          language of all parts of this Agreement will in all cases be construed
+          as a whole in accordance with its fair meaning and not for or against
+          either party.
+        </p>
+        <p>
+          (e) All notices provided for in this Agreement shall be given in
+          writing and shall be effective when either served by hand delivery,
+          electronic facsimile transmission, express overnight courier service,
+          or by registered or certified mail, return receipt requested,
+          addressed to the parties at their respective addresses as set forth at
+          the beginning of this Agreement, or to such other address or addresses
+          as either party may later specify by written notice to the other.
+        </p>
+      </section>
+
+      <section className="flex flex-col gap-4">
+        <h2 className="text-3xl font-bold mb-6">9. Work Product Ownership</h2>
+        <p>
+          (a) All work products, deliverables, or any other materials created by
+          Independent Contractor in the performance of the Services under this
+          Agreement (collectively, the "Work Product") shall be the sole
+          property of the Company. Independent Contractor hereby assigns and
+          agrees to assign to Company all rights, title, and interest in and to
+          the Work Product, including all intellectual property rights therein.
+          Independent Contractor agrees to execute any documents and take any
+          actions that may be necessary, or as Company may reasonably request,
+          to perfect the assignment of such rights to Company or to protect
+          Company's interest in such Work Product.
+        </p>
+        <p>
+          (b) Independent Contractor agrees that all Work Product shall be
+          considered “works made for hire” under the U.S. Copyright Act and that
+          Company shall be deemed the author of such Work Product. If, for any
+          reason, any of the Work Product does not constitute a “work made for
+          hire,” Independent Contractor hereby irrevocably assigns to Company
+          all right, title, and interest in and to such Work Product, including
+          all intellectual property rights therein, without additional
+          compensation. Independent Contractor shall promptly disclose to
+          Company any Work Product, and Independent Contractor shall cooperate
+          with Company and execute such documents as may be reasonably necessary
+          to assist Company in obtaining or protecting its rights in such Work
+          Product.
+        </p>
+      </section>
+    </section>
+  );
+}

396fb51eb69c91e593dae955b26f0759fd3ff3d2
GOGA-23 <gokulgandhi97@gmail.com>
2025-03-19 18:26:15 +0530

    t

--- components/Services.tsx
+++ components/Services.tsx
@@ -7,13 +7,13 @@ import { parentVariant, variantGrid2, variantTitle } from "@/libs/Variants";
 
 export default function Services() {
   return (
-    <section className="block space-y-10 md:space-y-20 p-10 sm:px-20 xl:px-64 sm:py-16 xl:py-28">
+    <section className="block space-y-10 md:space-y-10 p-10 sm:px-20 xl:px-60 sm:py-16 xl:py-28">
       <motion.h3
         variants={variantTitle}
         viewport={{ once: true }}
         initial="initial"
         whileInView="animate"
-        className=" text-[26px] lg:text-[56px] xl:text-[72px]  w-full  !leading-tight h-full font-EbGaramond  tracking-wider font-medium text-center"
+        className=" text-[26px] lg:text-[56px] xl:text-[72px]  w-full !leading-tight h-full font-EbGaramond  tracking-wider font-medium text-center "
       >
         Fueling Business with{" "}
         <span className="font-extrabold uppercase text-info">
@@ -27,27 +27,27 @@ export default function Services() {
         viewport={{ amount: 0.3, once: true }}
         initial="initial"
         whileInView="animate"
-        className="flex md:justify-center max-w-sm sm:flex-wrap sm:max-w-full gap-5 py-10"
+        className="flex md:justify-center max-w-sm sm:flex-wrap sm:max-w-full overflow-x-scroll md:overflow-x-auto gap-5 py-20"
       >
         {services.map((service, idx) => (
-          <motion.div
+          <motion.a
             variants={variantGrid2}
             initial="initial"
             whileInView="animate"
             custom={idx}
             viewport={{ once: true }}
             key={idx}
-            className="px-5 min-w-60 sm:w-52 sm:justify-center md:w-60 lg:w-[280px] border-4 border-[#DFE3E8] h-full sm:h-80 xl:h-96 py-14 hover:scale-125 transition-all duration-500 ease-in-out"
+            className="px-5 min-w-60 sm:w-52 sm:justify-center md:w-60 lg:w-[280px] border-4 border-[#DFE3E8] h-full sm:h-80 xl:h-96 py-14 xl:py-20 lg:hover:!scale-110 lg:hover:!rounded-lg transition-all duration-500 cursor-pointer"
           >
-            <Link href={service.href} className="block space-y-10">
+            <div className="block space-y-10">
               <div className="text-[#404040] text-lg tracking-wider sm:text-xl xl:text-3xl block space-y-1.5 text-center uppercase font-EbGaramond font-bold">
                 <h4 className="text-info scale-110">{service.title}</h4>
                 <h4>Essentials</h4>
               </div>
               <hr className="w-full h-1.5 bg-[#404040] rounded-md" />
               <TypeText service={service} />
-            </Link>
-          </motion.div>
+            </div>
+          </motion.a>
         ))}
       </motion.div>
     </section>
